74ffcdba059c3244614810e74ec50f27be3eaedb
pauloskaterock <latitude E6430@latitudeE6430>
2018-11-09 15:37:06 -0200

    hello_world

---
+++ Documents/NetBeansProjects/helloWorld_ph/src/helloworld_ph/HelloWorld_ph.java
@@ -0,0 +1,16 @@
+/*
+ PH_skateRock
+ */
+package helloworld_ph;
+
+/**
+ skateRock_ph
+ */
+public class HelloWorld_ph {
+
+    
+    public static void main(String[] args) {
+         System.out.println ("hello world PH");
+    }
+    
+}
